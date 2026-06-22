844ecff88a8afdc07f78f34b2801361aea4bc34d
roo7gb <miasma1007@gmail.com>
2026-06-22 09:48:57 -0400

    Add global catppuccin config

---
+++ nix/programs/catppuccinOS.nix
@@ -0,0 +1,30 @@
+# catppuccinOS.nix
+
+{
+  lib,
+  catppuccin,
+  ...
+}:
+{
+  catppuccin = {
+    autoEnable = false;
+    enable = true;
+    enableReleaseCheck = true;
+
+    flavor = "mocha";
+
+    cursors = {
+      enable = true;
+      accent = "sapphire";
+    };
+
+    gtk.icon = {
+      enable = true;
+      accent = "sapphire";
+    };
+
+    tty = {
+      enable = true;
+    };
+  }
+}
