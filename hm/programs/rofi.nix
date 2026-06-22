af84e09d93e9bfe834b5a3ffcbc29ed0aaf5e29c
roo7gb <miasma1007@gmail.com>
2026-06-22 19:48:23 -0400

    Edit rofi config for sidebar appearance

--- hm/programs/rofi.nix
+++ hm/programs/rofi.nix
@@ -5,41 +5,91 @@
   config,
   ...
 }:
+let
+  inherit (config.lib.formats.rasi) mkLiteral;
+in
 {
   programs.rofi = {
     enable = true;
+    package = pkgs.rofi;
 
     extraConfig = {
-      modi = "drun,run,window";
-      sidebar-mode = true;
+      modi = "drun";
       show-icons = true;
+      drun-display-format = "{name}";
+      disable-history = false;
+      hide-scrollbar = true;
     };
 
-    theme = let
-      inherit (config.lib.formats.rasi) mkLiteral;
-    in {
+    theme = {
       "*" = {
         background-color = mkLiteral "#11111b";
 	text-color = mkLiteral "#cdd6f4";
+	font = "Agave Nerd Font 10";
       };
+
       "window" = {
-        width = mkLiteral "600px";
+        anchor = mkLiteral "west";
+	location = mkLiteral "west";
+	height = mkLiteral "100%";
+	width = mkLiteral "300px";
+	x-offset = mkLiteral "0px";
+	y-offset = mkLiteral "0px";
+	children = map mkLiteral [ "mainbox" ];
       };
-      "mode-switcher" = {
-        orientation = mkLiteral "horizontal";
-	expand = false;
-	spacing = mkLiteral "10px";
-	background-color = mkLiteral "transparent";
+
+      "mainbox" = {
+        orientation = mkLiteral "vertical";
+	children = map mkLiteral [ "inputbar" "listview" ];
       };
-      "button" = {
-        padding = mkLiteral "10px";
-	border-radius = mkLiteral "5px";
-	background-color = mkLiteral "#1e1e2e";
-	text-color = mkLiteral "#cdd6f4";
+
+      "inputbar" = {
+        padding = mkLiteral "12px";
+	background-color = mkLiteral "#313244";
+	children = map mkLiteral [ "prompt" "entry" ];
       };
-      "button-selected" = {
+
+      "prompt" = {
+        margin = mkLiteral "0px 8px 0px 0px";
+	text-color = mkLiteral "#74c7ec";
+	background-color = mkLiteral "#313244";
+      };
+
+      "entry" = {
+        placeholder = "Search...";
+	placeholder-color = mkLiteral "#313244";
+      };
+
+      "listview" = {
+        columns = 1;
+	lines = 12;
+	spacing = mkLiteral "4px";
+	cycle = false;
+	dynamic = true;
+	layout = mkLiteral "vertical";
+      };
+
+      "element" = {
+        padding = mkLiteral "8px 12px";
+	spacing = mkLiteral "8px";
+	border-radius = mkLiteral "4px";
+      };
+
+      "element-text" = {
+	vertical-align = mkLiteral "0.5";
+      };
+
+      "element normal.normal" = {
+        background-color = mkLiteral "transparent";
+      };
+
+      "element selected.normal" = {
         background-color = mkLiteral "#74c7ec";
 	text-color = mkLiteral "#11111b";
+      };
+
+      "element-icon" = {
+        size = mkLiteral "24px";
       };
     };
   };
