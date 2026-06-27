38b8a353cfa72e42e9d5ae99c65b72e772da2fcd
roo7gb <miasma1007@gmail.com>
2026-06-27 00:17:11 -0400

    Remove anims from hyprpicker

--- hm/programs/hyprland/layerrules.nix
+++ hm/programs/hyprland/layerrules.nix
@@ -22,8 +22,8 @@
     (lib.generators.mkLuaInline ''{ match = { namespace = "rofi" }, animation = "fade" }'')
     (lib.generators.mkLuaInline ''{ match = { namespace = "awww" }, animation = "fade" }'')
 
-    (lib.generators.mkLuaInline ''{ match = { namespace = "hyprpicker" }, animation = "none" }'')
-    (lib.generators.mkLuaInline ''{ match = { namespace = "selection" }, animation = "none" }'')    
+    (lib.generators.mkLuaInline ''{ match = { namespace = "hyprpicker" }, no_anim = true }'')
+    (lib.generators.mkLuaInline ''{ match = { namespace = "selection" }, no_anim = true }'')
   ];
 }
 
