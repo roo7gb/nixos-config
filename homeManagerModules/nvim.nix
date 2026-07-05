7c69a19c312d52e552647548285d713e92b19701
roo7gb <miasma1007@gmail.com>
2026-07-05 09:21:18 -0400

    Add additional languages to nvim treesitter/lsp

--- homeManagerModules/nvim.nix
+++ homeManagerModules/nvim.nix
@@ -27,6 +27,32 @@
           enableTreesitter = true;
 
           nix.enable = true;
+          markdown.enable = true;
+
+          # enable based on needs
+
+          # assembly.enable = true;
+          # bash.enable = true;
+          # cmake.enable = true;
+          # csharp.enable = true;
+          # css.enable = true;
+          # docker.enable = true;
+          # go.enable = true;
+          # haskell.enable = true;
+          # html.enable = true;
+          # java.enable = true;
+          # json.enable = true;
+          # lua.enable = true;
+          # make.enable = true;
+          # python.enable = true;
+          # qml.enable = true;
+          # rust.enable = true;
+          # sql.enable = true;
+          # toml.enable = true;
+          # typescript.enable = true;
+          # xml.enable = true;
+          # yaml.enable = true;
+          # zig.enable = true;
         };
       };
     };
