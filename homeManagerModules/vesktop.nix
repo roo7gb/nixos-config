c0efd0f28136dbaa5f9f9a5ab8ba778dee42eed0
roo7gb <miasma1007@gmail.com>
2026-07-05 09:42:53 -0400

    Add to vencord config

--- homeManagerModules/vesktop.nix
+++ homeManagerModules/vesktop.nix
@@ -5,6 +5,20 @@
   # - CONFIG ------------------ #
   programs.vesktop = {
     enable = true;
+
+    settings = {
+      discordBranch = "stable";
+      minimizeToTray = false;
+      splash = true;
+    };
+
+    vencord.settings = {
+      notifyAboutUpdates = false;
+      autoUpdate = false;
+      plugins = {
+        volumeBooster.enabled = true;
+      };
+    };
   };
 }
 
